bc9bd35f9a88cf6307ce9ec4a6a5d047792db941
Charles Arboleda <charlesarboleda_@hotmail.com>
2022-11-18 00:37:22 +0000

    Refactored getAllReservations to use query searching

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -86,7 +86,23 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `;
+
+ 
+  const values = [guest_id, limit];
+
+  return pool.query(queryString, values)
+    .then(res => res.rows)
+    .catch(err => err);
 }
 exports.getAllReservations = getAllReservations;
 
